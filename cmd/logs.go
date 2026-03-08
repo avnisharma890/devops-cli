8c53c6fc28763c946d2400527988636ae63caf62
avni sharma <avnis7733@gmail.com>
2026-03-08 14:17:10 +0530

    docs: update logs command short description

--- cmd/logs.go
+++ cmd/logs.go
@@ -10,7 +10,7 @@ import (
 // logsCmd represents the logs command
 var logsCmd = &cobra.Command{
 	Use:   "logs [service]",
-	Short: "A brief description of your command",
+	Short: "Stream logs from services",
 	Args:  cobra.ExactArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
